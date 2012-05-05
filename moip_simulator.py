ed8e872b78938661d667a302450bbcc14be880b1
Romulo Jales <romulo.jales@corp.globo.com>
2012-05-05 10:34:17 -0300

    correcao gerais

--- moip_simulator.py
+++ moip_simulator.py
@@ -1,7 +1,7 @@
 import urllib
 import httplib
 
-SERVIDOR="http://www.amigosdobaleia.org.br"
+SERVIDOR="www.amigosdobaleia.org.br"
 URI="/donations/moip"
 
 def do_notify_post(id_transaction, email, status=5, cod_moip=12345678, parcelas=None,valor=5000 ,forma_pagamento=3,tipo_pagamento="CartaoDeCredito"):
@@ -13,9 +13,9 @@ def do_notify_post(id_transaction, email, status=5, cod_moip=12345678, parcelas=
                                 'forma_pagamento':forma_pagamento,
                                 'tipo_pagamento':tipo_pagamento,
                                 'status':status})
-    con = httplib.HTTPConnection(SERVIDOR+URI)
-    con.request("POST",params)
-    response = conn.getresponse()
+    con = httplib.HTTPConnection(SERVIDOR)
+    con.request("POST",URI,params)
+    response = con.getresponse()
     print response.status, response.reason
     data = response.read()
     print data
